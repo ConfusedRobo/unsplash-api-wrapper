5acb7b7636ba9437aacfaff32d242c05029951b0
noname_noob <74067540+ConfusedRobo@users.noreply.github.com>
2021-07-19 17:04:20 +0530

    Added documentations foe RandomPaths and TopPaths classes.

--- src/main/java/utils/paths/RandomPaths.java
+++ src/main/java/utils/paths/RandomPaths.java
@@ -5,7 +5,12 @@ import annotations.Author;
 import static utils.paths.TopPaths.RESOURCE_PATH;
 
 /**
+ * This interface is a list of all of the save paths that has been set by default and
+ * it also has all of the paths that are being utilised in the {@code UnsplashRandomCategory}
+ * class.
  *
+ * @author ConfusedRobo
+ * @see TopPaths
  */
 @Author(
         author = "ConfusedRobo",
@@ -13,8 +18,24 @@ import static utils.paths.TopPaths.RESOURCE_PATH;
         profile = "https://github.com/ConfusedRobo"
 )
 public interface RandomPaths {
+    /**
+     * Path to the cache directory where all of the {@code UnsplashRandomFixed}
+     * and {@code UnsplashRandomCategorised} class's json files will be saved
+     */
     String RAND_JSON_CACHE_SAVE_PATH = RESOURCE_PATH + "/jsoncaches/random";
+    /**
+     * Path to the random folder where all of the {@code UnsplashRandomFixed}
+     * and {@code UnsplashRandomCategorised} class's images files will be saved
+     */
     String RAND_IMAGE_SAVE_PATH = RESOURCE_PATH + "/random";
+    /**
+     * Path to the fixed folder where all of the images from {@code UnsplashRandomFixed}
+     * will be saved
+     */
     String RAND_IMAGE_FIXED_SAVE_PATH = RAND_IMAGE_SAVE_PATH + "/fixed";
+    /**
+     * Path to the fixed folder where all of the images from {@code UnsplashRandomCategorised}
+     * will be saved
+     */
     String RAND_CATEGORISED_SAVE_PATH = RAND_IMAGE_SAVE_PATH + "/categorised";
 }
