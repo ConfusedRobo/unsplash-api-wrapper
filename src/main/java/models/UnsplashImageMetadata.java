7679e8f0dd2c89bb2a5b9defdda89cbee1dab602
noname_noob <74067540+ConfusedRobo@users.noreply.github.com>
2021-07-29 06:08:33 +0530

    added documentations for UnsplashImageMetadata class

--- src/main/java/models/UnsplashImageMetadata.java
+++ src/main/java/models/UnsplashImageMetadata.java
@@ -1,78 +1,69 @@
 package models;
 
-import org.jetbrains.annotations.NotNull;
+import models.model_utils.Dimension;
 import org.json.JSONObject;
 
 import java.io.Serial;
 import java.io.Serializable;
-import models.model_utils.Dimension;
 
-import static java.lang.System.err;
 
+/**
+ * This class parses all of the metadata from the fetched JSON file. Note that, metadata in this
+ * context means all of the tertiary information related to an image. Like, blur hash, width, height,
+ * color profiles, creation date, modification date, etc.
+ * Also, this class's {@link #toJSON()} should be called first and other JSON objects added on top of
+ * it.
+ *
+ * @see Serializable
+ */
 @SuppressWarnings("SpellCheckingInspection")
 public class UnsplashImageMetadata implements Serializable {
     /**
      * The serial version number field that will assist the JVM to correctly
-     * cast/parse the object
+     * serialize/deserialize the object
      */
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private String blurhash;
-    private String color;
+    /**
+     * The {@code blur_hash} JSON field
+     */
+    public String blurhash;
+    /**
+     * The {@code color} JSON field
+     */
+    public String color;
 
-    private UnsplashImageDateTimeData createdAt;
-    private UnsplashImageDateTimeData updatedAt;
+    /**
+     * The {@code created_at} JSON field
+     */
+    public UnsplashImageDateTimeData createdAt;
+    /**
+     * The {@code updated_at} JSON field
+     */
+    public UnsplashImageDateTimeData updatedAt;
 
-    private Dimension dimension;
+    /**
+     * The {@code width} and {@code height} fields which are packed into one class {@link Dimension}
+     */
+    public Dimension dimension;
 
-    public void setBlurhash(String blurhash) { this.blurhash = blurhash; }
-
-    public void setColor(String color) {
-        if (!color.startsWith("#")) { err.println("Format not supported. Must be in HEX"); return; }
-        this.color = color;
-    }
-
-    public void setCreatedAt(UnsplashImageDateTimeData createdAt) { this.createdAt = createdAt; }
-
-    public void setUpdatedAt(UnsplashImageDateTimeData updatedAt) { this.updatedAt = updatedAt; }
-
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = new UnsplashImageDateTimeData(createdAt);
-    }
-
-    public void setUpdatedAt(String updatedAt) {
-        this.updatedAt = new UnsplashImageDateTimeData(updatedAt);
-    }
-
-    public void setDimension(@NotNull Dimension dimension) {
-        this.dimension = dimension;
-    }
-
-    public void setDimension(int width, int height) {
-        this.dimension = new Dimension(width, height);
-    }
-
+    /**
+     * This method packs all of the parsed or, manually assigned data to unsplash.com API compliant
+     * JSON fragment that can be used build up the whole JSON.
+     *
+     * @return a {@link JSONObject} object
+     */
     public JSONObject toJSON() {
         var jsonBuilder = new JSONObject();
-        jsonBuilder.put("blurhash", this.blurhash);
+        jsonBuilder.put("blur_hash", this.blurhash);
         jsonBuilder.put("color", this.color);
         jsonBuilder.put("created_at", this.createdAt);
         jsonBuilder.put("updated_at", this.updatedAt);
         jsonBuilder.put("width", dimension.width);
         jsonBuilder.put("height", dimension.height);
-        jsonBuilder.put("creation_date", this.createdAt.date());
-        jsonBuilder.put("updation_date", this.updatedAt.date());
+        jsonBuilder.put("created_at", this.createdAt.date());
+        jsonBuilder.put("updated_at", this.updatedAt.date());
         return jsonBuilder;
     }
-
-    public String getBlurhash() { return blurhash; }
-
-    public String getColor() { return color; }
-
-    public UnsplashImageDateTimeData getCreatedAt() { return createdAt; }
-
-    public UnsplashImageDateTimeData getUpdatedAt() { return updatedAt; }
-
-    public Dimension getDimension() { return dimension; }
 }
